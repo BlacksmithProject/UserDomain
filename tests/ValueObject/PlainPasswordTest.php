7b393f1cef993eff491a4abad2e2e7e681622b44
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-02-21 07:43:47 +0100

    feat(UserRegistration): add PlainPassword

---
+++ tests/ValueObject/PlainPasswordTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace BSP\User\Tests\ValueObject;
+
+use Assert\InvalidArgumentException;
+use BSP\User\ValueObject\PlainPassword;
+use PHPUnit\Framework\TestCase;
+
+final class PlainPasswordTest extends TestCase
+{
+    public function testItCanBeInitialized(): void
+    {
+        $plainPassword = new PlainPassword('winter is coming');
+
+        $this->assertSame('winter is coming', $plainPassword->value());
+    }
+
+    public function testItCannotBeInitializedWithNull(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('user.password.must_not_be_null');
+
+        new PlainPassword(null);
+    }
+
+    public function testItCannotBeInitalizedWithBlankString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('user.password.must_not_be_blank');
+
+        new PlainPassword('');
+    }
+}
