a775a20172df2fabd66783e311e11ce517b09099
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-02-21 18:44:26 +0100

    feat(CredentialsRegistration): add credentialsId into command

--- src/Command/Registration/RegisterCredentials.php
+++ src/Command/Registration/RegisterCredentials.php
@@ -3,18 +3,26 @@ declare(strict_types=1);
 
 namespace BSP\Credentials\Command\Registration;
 
+use BSP\Credentials\ValueObject\CredentialsId;
 use BSP\Credentials\ValueObject\Email;
 use BSP\Credentials\ValueObject\PlainPassword;
 
 final class RegisterCredentials
 {
+    private $credentialsId;
     private $email;
     private $plainPassword;
 
-    public function __construct(?string $email, ?string $plainPassword)
+    public function __construct(CredentialsId $credentialsId, ?string $email, ?string $plainPassword)
     {
+        $this->credentialsId = $credentialsId;
         $this->email = new Email($email);
         $this->plainPassword = new PlainPassword($plainPassword);
+    }
+
+    public function credentialsId(): CredentialsId
+    {
+        return $this->credentialsId;
     }
 
     public function email(): Email
