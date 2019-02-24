07d30b898fa0201f0d91a34b25c53f39c4d61f3f
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-02-24 07:31:29 +0100

    feat(CredentialsRegistration): add FindCredentials withId Query

--- src/Port/CredentialsReader.php
+++ src/Port/CredentialsReader.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace BSP\Credentials\Port;
 
+use BSP\Credentials\Entity\Credentials;
+use BSP\Credentials\ValueObject\CredentialsId;
 use BSP\Credentials\ValueObject\Email;
 
 interface CredentialsReader
 {
     public function isEmailAlreadyUsed(Email $email): bool;
+
+    public function get(CredentialsId $credentialsId): Credentials;
 }
